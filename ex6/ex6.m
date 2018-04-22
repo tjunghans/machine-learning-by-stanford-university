89d96b1daa8b05535a88f80f4662c2ea6f3b95a1
Thomas Junghans <thomas.junghans@gmail.com>
2018-04-22 20:08:43 +0200

    feat: add 1.2.1 solution

--- ex6/ex6.m
+++ ex6/ex6.m
@@ -32,10 +32,10 @@ fprintf('Loading and Visualizing Data ...\n')
 load('ex6data1.mat');
 
 % Plot training data
-plotData(X, y);
+%_plotData(X, y);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%_fprintf('Program paused. Press enter to continue.\n');
+%_pause;
 
 %% ==================== Part 2: Training Linear SVM ====================
 %  The following code will train a linear SVM on the dataset and plot the
@@ -44,18 +44,18 @@ pause;
 
 % Load from ex6data1: 
 % You will have X, y in your environment
-load('ex6data1.mat');
+%_load('ex6data1.mat');
 
-fprintf('\nTraining Linear SVM ...\n')
+%_fprintf('\nTraining Linear SVM ...\n')
 
 % You should try to change the C value below and see how the decision
 % boundary varies (e.g., try C = 1000)
-C = 1;
-model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
-visualizeBoundaryLinear(X, y, model);
+C = 100;
+%_model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
+%_visualizeBoundaryLinear(X, y, model);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%_fprintf('Program paused. Press enter to continue.\n');
+%_pause;
 
 %% =============== Part 3: Implementing Gaussian Kernel ===============
 %  You will now implement the Gaussian kernel to use
@@ -69,6 +69,7 @@ sim = gaussianKernel(x1, x2, sigma);
 fprintf(['Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = %f :' ...
          '\n\t%f\n(for sigma = 2, this value should be about 0.324652)\n'], sigma, sim);
 
+return;
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
